b99aedb37e8096665813d8f8eda851aa9d3a3145
hunny-A <nahyun7656a@gmail.com>
2023-01-17 01:08:20 +0900

    searching 기능

--- app.py
+++ app.py
@@ -55,5 +55,11 @@ def web_record_get():
     record_list = list(db.SMTP_DB.find({}, {'_id': False}))
     return jsonify({'records':record_list})
 
+# 검색기능
+@app.route("/record_search", methods=["GET"])
+def web_searching():
+    search_list = list(db.SMTP_DB.find({}, {'_id': False}))
+    return jsonify({'searching':search_list})
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
